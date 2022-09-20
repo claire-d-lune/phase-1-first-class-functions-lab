7ccacdfe14756ca876b0be7a6a08a19a8eda1add
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-19 21:06:01 -0600

    commit tree df53f4a2c62606f2a9f4f8098bc00e74102142b0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const danivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = (drivers) => {
+    let firstTwo = [drivers[0], drivers[1]];
+    return firstTwo;
+};
+
+const returnLastTwoDrivers = (drivers) => {
+    let arrayLength = drivers.length; 
+    let lastTwo = [drivers[(arrayLength - 2)], drivers[(arrayLength - 1)]];
+    return lastTwo;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (multiplyValue) {
+    return function (value){
+        return multiplyValue * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (names, selection) {
+    return selection(names)
+}
+
+
+
